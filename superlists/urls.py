d4a121edf4c2132be0aa0a0dcfde92c0f71548f9
Diego G Plata <diegogplata@gmail.com>
2021-01-27 18:17:21 -0600

    ✅add first passing unit test📝update notes.txt✨map root route to home_page view function✨add home_page view function

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path for root route
+    path('', views.home_page, name='home')
 ]

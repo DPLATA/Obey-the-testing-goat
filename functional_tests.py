f94a14632e1a4926770daa7cc10cc1433f56f488
Diego G Plata <diegogplata@gmail.com>
2021-01-29 11:09:38 -0600

    ♻️remove duplicates refactor in FT✅update FT to add helper method🚧work in progress

--- functional_tests.py
+++ functional_tests.py
@@ -21,6 +21,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.close()
         self.browser.quit()
 
+    # helper method will not be run as test because does not start with test
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -38,18 +44,16 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # Error message at the end of the line
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        #self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-        #f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        #)
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
         self.fail('Finish the test')
 
 

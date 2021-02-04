1fc608209e6b4d1c318f5471e3283b08ce7a936a
Diego G Plata <diegogplata@gmail.com>
2021-02-04 08:31:21 -0600

    ♻️🚧refactor WIP

--- lists/urls.py
+++ lists/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from lists import views as list_views
-from lists import urls as list_urls
+from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path for root route
-    path('', list_views.home_page, name='home'),
-    re_path(r'^lists/', include(list_urls)),
+    path('', views.home_page, name='home'),
+    path('lists/new', views.new_list, name='new_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item')
 ]
